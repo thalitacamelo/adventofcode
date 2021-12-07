cedb78b134b539d83072dcecd05f91489a73aea4
thalitacamelo <thalitacamelofar@gmail.com>
2021-12-07 02:54:18 -0300

    Day7

---
+++ 2021/Day7/TreacheryofWhales_part1.py
@@ -0,0 +1,11 @@
+import sys
+
+with open('sample.txt', 'r') as file:
+    positions = list(map(int, file.readline().split(',')))
+
+def cost (i):
+    return sum(abs(p - i) for p in positions)
+
+maxposition = max(positions)
+
+print(min(map(cost, range(maxposition))))
