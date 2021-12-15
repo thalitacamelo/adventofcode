d68c4e270b3dfd6430b9c52e7529a520bc9d9e95
thalitacamelo <thalitacamelofar@gmail.com>
2021-12-15 03:39:41 -0300

    Day14

--- 2021/Day14/ExtendedPolymerization_part2.py
+++ 2021/Day14/ExtendedPolymerization_part2.py
@@ -6,31 +6,25 @@ with open('input.txt', 'r') as file:
     rules = [line.strip() for line in file.readlines() if line]
     rules = dict(line.split(' -> ') for line in rules)
 
-count_elements = {}
-for value in rules.values():
-    if value not in count_elements:
-        count_elements[value] = 0
+count_elements = dict((value, 0) for value in rules.values())
 
-count_pairs = {}
-for key in rules.keys():
-    if key not in count_pairs:
-        count_pairs[key] = 0
+count_pairs = dict((key, 0) for key in rules.keys())
 
 def polymerization(template, rules, count_pairs, count_elements, steps):
     for i in range(len(template) - 1):
         count_pairs[template[i:i+2]] += 1
-    for i in range(len(template)):
-        count_elements[template[i]] += 1
+
+    for letter in template:
+        count_elements[letter] += 1
+        
     for i in range(steps):
-        temp = count_pairs.copy()
-        for key in temp.keys():
-            j = temp[key]
-            pair = key
-            insert = rules[key]
-            count_elements[insert] += 1 * j
-            count_pairs[key] -= 1 * j
-            count_pairs[pair[0]+insert] += 1 * j
-            count_pairs[insert+pair[1]] += 1 * j           
+        new_count_pairs = dict((key, 0) for key in rules.keys())
+        for pair, count in count_pairs.items():
+            insert = rules[pair]
+            count_elements[insert] += count
+            new_count_pairs[pair[0]+insert] += count
+            new_count_pairs[insert+pair[1]] += count
+        count_pairs = new_count_pairs          
 
 polymerization(template, rules, count_pairs, count_elements, 40)
 
